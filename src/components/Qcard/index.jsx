2fbbb8a9840d2635c4b52e4dfb575bebd14d09ac
ashishkumarui6 <ashishkumarui6@gmail.com>
2024-12-14 01:03:03 +0530

    logic implement

--- src/components/Qcard/index.jsx
+++ src/components/Qcard/index.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import styles from "./index.module.css";
-const Qcard = () => {
+const Qcard = ({ Sn, heading, A, B, C, D, isSelected, onGetAnsawer }) => {
   return (
     <div className={styles.Qcard}>
       <div className={styles.left}>
         <div className={styles.serial_num}>
-          <p>1.</p>
+          <p>{Sn}.</p>
         </div>
-        <h1>Which is not valid data type in Javascript?</h1>
+        <h1>{heading}</h1>
       </div>
       <div className={styles.right}>
-        <div className={styles.Question1}>
-          <input type="radio" name="1" />
+        <div onClick={() => onGetAnsawer(Sn, A)} className={styles.Question1}>
+          <input type="radio" name={heading} checked={isSelected === A} />
           <p>A.</p>
-          <label>Undefinded</label>
+          <label>{A}</label>
         </div>
-        <div className={styles.Question2}>
-          <input type="radio" name="1" />
+        <div onClick={() => onGetAnsawer(Sn, B)} className={styles.Question2}>
+          <input type="radio" name={heading} checked={isSelected === B} />
           <p>B.</p>
-          <label>Undefinded</label>
+          <label>{B}</label>
         </div>
-        <div className={styles.Question3}>
-          <input type="radio" name="1" />
+        <div onClick={() => onGetAnsawer(Sn, C)} className={styles.Question3}>
+          <input type="radio" name={heading} checked={isSelected === C} />
           <p>C.</p>
-          <label>Undefinded</label>
+          <label>{C}</label>
         </div>
-        <div className={styles.Question4}>
-          <input type="radio" name="1" />
+        <div onClick={() => onGetAnsawer(Sn, D)} className={styles.Question4}>
+          <input type="radio" name={heading} checked={isSelected === D} />
           <p>D.</p>
-          <label>Undefinded</label>
+          <label>{D}</label>
         </div>
       </div>
     </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.css";
 import Container from "../../container";
 import Qcard from "../../components/Qcard";
+import { quizeData } from "../../data/data";
 
 const Home = () => {
+  const [isSelected, setIsSelected] = useState({});
+
+  const onGetAnsawer = (qNo, opt) => {
+    setIsSelected((prev) => ({ ...prev, [qNo]: opt }));
+  };
+
+  const onSubmitAnswer = () => {
+    const filterData = quizeData.filter(
+      (it) => isSelected[it.Sn] === it.answer
+    );
+    console.log(filterData.length + "/" + quizeData.length);
+  };
+
   return (
     <Container>
       <div className={styles.Home}>
@@ -12,34 +26,23 @@ const Home = () => {
           <div className={styles.right}>Interview MCQ's</div>
         </div>
         <div className={styles.bot}>
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
-          <Qcard />
+          {quizeData.map((it) => {
+            return (
+              <Qcard
+                Sn={it.Sn}
+                heading={it.heading}
+                A={it.aLabel}
+                B={it.bLabel}
+                C={it.cLabel}
+                D={it.dLabel}
+                isSelected={isSelected[it.Sn]}
+                onGetAnsawer={onGetAnsawer}
+              />
+            );
+          })}
+        </div>
+        <div className={styles.btn}>
+          <button onClick={onSubmitAnswer}>Submit</button>
         </div>
       </div>
     </Container>
